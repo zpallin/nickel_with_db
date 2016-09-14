193f6e3e6736fc37ed6be2d7e0b70bf87431c610
zpallin <zpallin@gmail.com>
2016-09-14 08:59:05 -0700

    Got it working with db and "some" instructions. Used a bit of structs and methods... currently fighting with borrow checker tho

---
+++ README.md
@@ -0,0 +1,24 @@
+SETUP
+=====
+
+I have some manual steps for setting up the DB unfortunately.
+
+1. You need to clone the following repo for the db. I recommend cloning into another directory.
+   `git clone --recursive https://github.com/pedroamador/ubuntu1404-mongodb26`
+2. Inside the `ubuntu1404-mongodb26` directory, I recommend changing the IP address to `192.168.33.10` since that address is hard coded in.
+3. You need to `vagrant ssh` and then edit `/etc/mongod.conf` so that `bind_ip = 192.168.33.10`.
+
+Once that is done, you should be able to connect with the db via the app.
+
+Running the App
+===============
+
+Three commands
+1. cargo run -- build <dbname> <collectionname>
+   Builds the db and collection in mongo
+2. cargo run -- add <dbname> <collectionnanme> <value>
+   Adds a simple BSON value in mongodb
+3. cargo run -- use <dbname> <collectionname>
+   Doesn't change the db, just runs it
+
+If you enter these wrong, it should error out.
--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,97 @@
  *  to create a web application of sufficient complexity
  */
 
+// mongo crates
 #[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
 extern crate time;
 
-use time::Timespec;
+// nickel crates
+#[macro_use] extern crate nickel;
+extern crate nickel_mustache;
+extern crate rustc_serialize;
+
+// argv uses
 use std::env;
+
+// mongo uses
+use bson::Bson;
 use mongodb::{Client, ThreadedClient};
-use mongodb::db::ThreadedDatabase;
+use mongodb::coll::Collection;
+use mongodb::db::{ThreadedDatabase, Database};
 use mongodb::db::options::CreateCollectionOptions;
+//use time::Timespec;
+
+// nickel uses
+//use nickel_mustache::Render;
+use nickel::{Nickel, HttpRouter};
 
 /*
- * objects defined for database experiment
+ * data object for passing around data
  */
-#[derive(Debug)]
-struct Poll {
-    id: i32,
-    question: String,
-    time_created: Timespec,
+struct DataObject {
+    client: Client,
+    db_name: String,
+    coll_name: String,
 }
 
-#[derive(Debug)]
-struct Vote {
-    id: i32,
-    name: String,
-    vote: bool,
-    time_created: Timespec,
+impl DataObject {
+
+    fn coll_name(&self) -> String { 
+        self.coll_name.clone()
+    }
+
+    fn db_name(&self) -> String {
+        self.db_name.clone()
+    }
+
+    fn get_db(&self) -> Database {
+        self.client.db(&self.db_name)
+    }
+
+    fn get_coll(&self) -> Collection {
+        self.get_db().collection(&self.coll_name)
+    }
+
+    fn add_value(&self, value: String) {
+        self.get_coll().insert_one(doc!{ "value" => value },None).unwrap();
+    }
+
+    fn build(&self) {
+        self.get_db()
+            .create_collection(&self.coll_name(), Some(db_options()))
+            .ok().expect(&format!("Failed to create {} collection", &self.coll_name));
+    }
+
+    fn list(&self) -> Vec<String> {
+
+        let coll = self.get_coll();
+        let cursor = coll.find(None, None).unwrap();
+        let mut list = Vec::new();
+
+        for result in cursor {
+            if let Ok(item) = result {
+                if let Some(&Bson::String(ref value)) = item.get("value") {
+
+                    // if let statement won't allow us to move "question"
+                    // so we need to clone it
+                    println!("{}", value);
+                    list.push(value.clone());
+                } else if let Some(&Bson::String(ref value)) = item.get("id") {
+                    println!("{} doesn't have question", value);
+                } else {
+                    println!("Nada");
+                }
+            }
+        }
+        list
+    }
 }
 
+/*
+ * functions used for interacting with db
+ */
 fn db_options() -> CreateCollectionOptions {
     let mut options = CreateCollectionOptions::new();
     options.capped = true;
@@ -42,28 +104,87 @@ fn db_options() -> CreateCollectionOptions {
     options
 }
 
-fn build(c: Client) {
-    println!("Lol build");
-    let db = c.db("pollsapp");
-
-    db.create_collection("polls", Some(db_options()))
-        .ok().expect("Failed to create polls collection");
-    
-    db.create_collection("votes", Some(db_options()))
-        .ok().expect("Failed to create votes collection");
+fn instructions(error_code: i32) {
+    println!("Wrong input (sorry no instructions yet)");
+    std::process::exit(error_code);
 }
 
-fn list(c: Client) {
-    let db = c.db("pollsapp");
- 
-}
+fn handle_args(c: Client, args: &Vec<String>) -> DataObject {
+    let mut database = DataObject{ 
+        client: c,
+        db_name: "".to_owned(),
+        coll_name: "".to_owned() 
+    };
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    let client = Client::connect("192.168.33.10", 27017)
-        .ok().expect("Failed to initialize client.");
+//    let build_val = "build".to_owned();
+//    let add_val = "add".to_owned();
 
-    if args.len() > 1 && args[1] == "build" {
-        build(client);
+
+    if args.len() > 1 {
+        let command: &str = &args[1];
+        match command {
+            "build" => {
+                if args.len() == 4 {
+                    println!("Building...");
+                    println!(" - {}.{}", args[2], args[3]);
+                    database.db_name = args[2].clone().to_owned();
+                    database.coll_name = args[3].clone().to_owned();
+                    database.build()
+                } else {
+                    instructions(1);
+                }
+            },
+            "add" => {
+                if args.len() == 5 {
+                    println!("Adding...");
+                    database.db_name = args[2].clone().to_owned();
+                    database.coll_name = args[3].clone().to_owned();
+                    println!(" - {}.{} value: {}", 
+                             database.db_name, 
+                             database.coll_name,
+                             args[4].clone());
+                    database.add_value(args[4].clone());
+                } else {
+                    instructions(1);
+                }
+            },
+            "use" => {
+                if args.len() == 4 {
+                    println!("Using...");
+                    println!(" - {}.{}", args[2], args[3]);
+                    database.db_name = args[2].clone().to_owned();
+                    database.coll_name = args[3].clone().to_owned();
+                } else {
+                    instructions(1);
+                }
+            },
+            _ => instructions(1),
+        }
     }
+    database
+}
+
+fn main() { 
+    let client = Client::connect("192.168.33.10", 27017)
+                 .ok().expect("Failed to initialize client.");
+    let args: Vec<_> = env::args().collect();
+    let mut server = Nickel::new();
+    let database = handle_args(client, &args);
+
+    server.get("/", middleware! { |_req, res|
+        
+        #[derive(RustcEncodable)]
+        struct ViewData<'a> {
+            title: &'a str,
+            values: &'a Vec<String>
+        }
+
+        let db_coll_name = format!("{}.{}",
+                                   database.db_name,
+                                   database.coll_name);
+        let data = ViewData{ title: &db_coll_name, values: &database.list() };
+        return res.render("templates/list.tpl", &data)
+    });
+
+    server.listen("localhost:6767");
 }
